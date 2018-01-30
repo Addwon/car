980e9344000b4708af47f4a92c17a92ecf5b8433
Addis <addwon@gmail.com>
2018-01-30 10:28:33 -0500

    Commit 2

--- src/main/java/assignments/car/CarApp.java
+++ src/main/java/assignments/car/CarApp.java
@@ -10,14 +10,24 @@ public class CarApp {
         c.setColor("red");
         c.setBrand("Porsche");
         c.setSpeed(50);
-        System.out.println("The " + c.getColor() + " " + c.getBrand() + " is " + c.start());
-        System.out.println("The " + c.getColor() + " " + c.getBrand() + " is " + c.accelerate());
-        System.out.println("The " + c.getColor() + " " + c.getBrand() + " is " + c.going()+" "+c.getSpeed()+" mph");
-        System.out.println("The " + c.getColor() + " " + c.getBrand() + " is " + c.stop());
+
+        Car c2=new Car();
+        c2.setColor("blue");
+        c2.setBrand("Jetta");
+
+
 
         return "The " + c.getColor() + " " + c.getBrand() + " is " + c.start()+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " is " + c2.start()+"<br/>"+
                 "The " + c.getColor() + " " + c.getBrand() + " is " + c.accelerate()+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " is " + c2.accelerate()+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " has " + c2.stop()+" to have its emissions checked"+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " passes its emmision test"+"<br/>"+
                 "The " + c.getColor() + " " + c.getBrand() + " is " + c.going()+" "+c.getSpeed()+" mph"+"<br/>"+
-                "The " + c.getColor() + " " + c.getBrand() + " is " + c.stop();
+                "The " + c.getColor() + " " + c.getBrand() + " is " + c.stop()+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " is " + c2.stop()+"<br/>"+
+                "The " + c2.getColor() + " " + c2.getBrand() + " passed the "+c.getColor()+" "+c.getBrand();
+
     }
+
 }
